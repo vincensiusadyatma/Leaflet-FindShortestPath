28d31ecf0d8c5b20d04b4d80ea4054a21e95e7b8
narwa <narwandaru.nara@gmail.com>
2024-06-05 17:47:15 +0700

    test: local test files for the algorithm and full data

--- src/js/testAlgo.js
+++ src/js/testAlgo.js
@@ -11,15 +11,17 @@ fullVertices.forEach(vertexData => {
     graph.createVertex(vertexData.id, vertexData.vertexType, vertexData.latitude, vertexData.longitude, vertexData.label, vertexData.neighborIds, true);
 });
 
+// const testVertexId = "rs-dr-sardjito"; console.log(`[DEBUGGING] neighbors of ${testVertexId}`); console.log(graph.getVertex(testVertexId).getNeighborIds()); throw new Error("Stop here");
 // console.log(graph.getVertices());
 
 // console.log(graph.getVertex("itc-1").getNeighborIds());
 
 const algorithm = 'greedy';
 
-const result = graph.computeShortestRoute("itc-33", "rs-jih", algorithm);
+const result = graph.computeShortestRoute("itc-33", "rs-siloam-yogyakarta", algorithm);
 if (result == []) {
     console.log('No path found');
 }
 console.log(`Algorithm: ${algorithm}`);
 console.log(result.toJSON());
+result.printRoutes();
