596ca1e14b05c89ab86dd7b2a1191c4d80121416
narwa <narwandaru.nara@gmail.com>
2024-06-08 19:58:05 +0700

    refactor: getTotalDistance in Results to return KM values

--- src/js/classes/Result.js
+++ src/js/classes/Result.js
@@ -15,9 +15,11 @@ class Result {
 
     getTotalDistance() {
         let totalDistance = 0;
-        this.#pathRoutes.forEach(path => {
-            totalDistance += path.cost;
-        });
+        for (let i = 1; i < this.#pathRoutes.length; i++) {
+            const currVertex = this.#pathRoutes[i].vertex;
+            const prevVertex = this.#pathRoutes[i - 1].vertex;
+            totalDistance += currVertex.haversineDistanceFrom(prevVertex);
+        }
         return totalDistance;
     }
 
